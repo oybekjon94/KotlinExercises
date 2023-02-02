95ca9461038a3f609a34a78e4cbd52d8abcbb4dd
oybekjon94 <x.oybek94@gmail.com>
2023-02-03 06:48:23 +0900

    [feat::]Add 2 practice

---
+++ src/main/kotlin/practice/2.kt
@@ -0,0 +1,15 @@
+package practice
+
+//fun main(){
+//    val fullName = 12.2
+//    println(message = fullName::class)
+//}
+// output: double
+fun main(){
+    var input: String?
+
+    print("enter something: ")
+    input = readLine()
+
+    println(input)
+}
