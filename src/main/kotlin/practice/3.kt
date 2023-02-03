cd62eec58fb18f6ee0fa8d7467a27f30bbea71bd
oybekjon94 <x.oybek94@gmail.com>
2023-02-03 20:58:19 +0900

    [feat::]Add 4 practice

---
+++ src/main/kotlin/practice/3.kt
@@ -0,0 +1,28 @@
+package practice
+
+//class Person {
+//    var name:String = ""
+//    var age: Int = 0
+//
+//    override fun toString(): String{
+//        return "person(name='$name', age=$age)"
+//    }
+//}
+//fun main(){
+//    val p1 = Person()
+//    p1.name = "Peter"
+//    p1.age = 23
+//
+//    println(p1)
+//}
+fun main() {
+
+    println(2)
+    println(+2)
+    println(-2)
+
+    val a = 1
+
+    println(-a)
+    println(-(-a))
+}
---
+++ src/main/kotlin/practice/4.kt
@@ -0,0 +1,25 @@
+package practice
+
+import java.util.*
+
+//fun main(){
+//    val words = arrayOf("kind","massive","atom","car","blue")
+//
+//    Arrays.sort(words) {s1: String, s2:String -> s1.compareTo(s2)}
+//
+//    println(Arrays.toString(words))
+//}
+fun main(){
+    var a:Byte = 126
+
+    println(a)
+    a++
+
+    println(a)
+    a++
+
+    println(a)
+    a++
+
+    println(a)
+}
