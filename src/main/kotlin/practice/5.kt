1bcc8647eac122c83175786c82f2ccd25f8c4ec2
oybekjon94 <x.oybek94@gmail.com>
2023-02-04 09:56:53 +0900

    [feat::]Add 5 practice

---
+++ src/main/kotlin/practice/5.kt
@@ -0,0 +1,27 @@
+package practice
+
+fun main(){
+//    val input:String?
+//
+//    print("enter something")
+//    input = readLine()
+//
+//    println(input)
+//------------------------------------
+//    println("return"+"of"+"the king")
+//    println("return".plus("of").plus("the king"))
+//-----------------------------------
+//    val words = listOf("forecast","bible","sky")
+//
+//    var nOfChars: Int = 0
+//
+//    for (word in words){
+//        val n = word!!.length
+//        nOfChars += n
+//    }
+//    println("there are ${nOfChars} characters in the list")
+//--------------------------------------------
+    var j = 0
+    j *= 3+1 // 0 * 3 + 1
+     println(j)
+}
