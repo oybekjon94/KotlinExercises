863d139e83c599950b04360f31e56493ca0b119d
oybekjon94 <x.oybek94@gmail.com>
2023-02-08 17:19:18 +0900

    [feat::]Add 6 array

---
+++ src/main/kotlin/string/6.kt
@@ -0,0 +1,12 @@
+package string
+
+//kotlin string case
+fun main(){
+    val s = "young eagle"
+
+    println(s.capitalize())
+    println(s.toUpperCase())
+    println(s.lowercase())
+
+    println("Hornet".decapitalize())
+}
